98641d6a233fc6d8bef6a4c3f8fe74e72dd133f9
zemlyansky <anton.zemlyansky@gmail.com>
2019-03-02 18:48:38 +0200

    Upd iframer

--- content/iframer.py
+++ content/iframer.py
@@ -1,9 +1,10 @@
 from IPython.core.display import display, HTML
 
-def open (link):
+def iframer (link):
+  site = link.split('/')[2]
   display(HTML("""
   <div style="position: relative; padding-bottom: 550px; padding-top: 25px; height: 0;">
     <iframe src="{link}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
   </div><br>
-  <a href="{link}" style="margin: 10px 0">Open in StatSim</a>
-  """.format(link=link)))
+  <a href="{link}" style="margin: 10px 0">Open in {site}</a>
+  """.format(link=link, site=site)))
