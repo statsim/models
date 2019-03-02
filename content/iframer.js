52d3fa95d32b0773660885bbf6c60ecf5592c2da
zemlyansky <anton.zemlyansky@gmail.com>
2019-03-02 19:23:02 +0200

    Upd iframe

--- content/iframer.js
+++ content/iframer.js
@@ -1,8 +1,8 @@
 module.exports = function iframer (link) {
   $$.html(`
-    <div style="position: relative; padding-bottom: 550px; padding-top: 25px; height: 0;">
+    <div class="iframer-wrapper" style="position: relative; padding-bottom: 550px; padding-top: 25px; height: 0;">
       <iframe src="${link}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
     </div><br>
-    <a href="${link}" style="margin: 10px 0">Open in ${link.split('/')[2]}</a>
+    <a class="iframer-link" href="${link}" target="_blank" style="margin: 10px 0;">Open in ${link.split('/')[2]}</a>
   `)
 }
--- content/iframer.py
+++ content/iframer.py
@@ -3,8 +3,8 @@ from IPython.core.display import display, HTML
 def iframer (link):
   site = link.split('/')[2]
   display(HTML("""
-  <div style="position: relative; padding-bottom: 550px; padding-top: 25px; height: 0;">
+  <div class="iframer-wrapper" style="position: relative; padding-bottom: 550px; padding-top: 25px; height: 0;">
     <iframe src="{link}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
   </div><br>
-  <a href="{link}" style="margin: 10px 0">Open in {site}</a>
+  <a class="iframer-link" href="{link}" target="_blank" style="margin: 10px 0;">Open in {site}</a>
   """.format(link=link, site=site)))
